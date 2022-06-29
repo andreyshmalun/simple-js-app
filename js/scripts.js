05cfb01e32d054f54c071eda4e4ce15d5f89606b
Andrey <shmalunandrey@gmail.com>
2022-06-28 21:33:20 -0400

    Create array with Pokémon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Nuzleaf', height: 3.03, types: ['grass', 'dark'] },
+  { name: 'Charmander', height: 2, types: 'fire' },
+  { name: 'Bulbasaur', height: 2.04, types: ['grass', 'poison'] },
+  { name: 'Squirtle', height: 1.08, types: 'water' },
+  { name: 'Metapod', height: 2.04, types: ['fire', 'flying', 'rock'] },
+];
