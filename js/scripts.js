d291e69ff4a61e71f9d72b337dd385fa21dd2da1
Andrey <shmalunandrey@gmail.com>
2022-06-30 12:38:39 -0400

    Update
    Create a for loop that iterates over each item in pokemonList, add a conditional.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 let pokemonList = [
   { name: 'Nuzleaf', height: 3.03, types: ['grass', 'dark'] },
   { name: 'Charmander', height: 2, types: 'fire' },
-  { name: 'Bulbasaur', height: 2.04, types: ['grass', 'poison'] },
-  { name: 'Squirtle', height: 1.08, types: 'water' },
-  { name: 'Metapod', height: 2.04, types: ['fire', 'flying', 'rock'] },
+  { name: 'Bulbasaur', height: 2.04, types: ['grass', 'poison'] }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 3) {
+    document.write(`<p> ${pokemonList[i].name}  (height: ${pokemonList[i].height}) - Wow, that's big! </p>`);
+  } else {
+    document.write(` <p>${pokemonList[i].name}  (height: ${pokemonList[i].height})</p>`);
+  }
+}
