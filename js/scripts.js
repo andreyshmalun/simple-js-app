41305fbb3ae10699fceebe9bee0ce0ecc40b4f6c
Andrey <shmalunandrey@gmail.com>
2022-07-19 16:36:08 -0400

    Fix all linting issues.

--- js/scripts.js
+++ js/scripts.js
@@ -24,42 +24,49 @@ let pokemonRepository = (function () {
     buttonItem.setAttribute('data-target', '#pokemon-modal');
     pokemonItem.appendChild(buttonItem);
     pokemonList.appendChild(pokemonItem);
-    buttonItem.addEventListener("click", function (event) {
+    buttonItem.addEventListener('click', function (event) {
       showDetails(pokemon);
     });
   }
 
-
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
       });
-    }).catch(function (e) {
-      console.error(e);
-    });
   }
 
   function loadDetails(item) {
     let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      //Add the details to the item
-      item.imageUrl = details.sprites.other.dream_world.front_default;
-      item.height = details.height;
-      item.weight = details.weight;
-      item.types = details.types.map((type) => type.type.name).join(', ');
-      item.abilities = details.abilities.map((ability) => ability.ability.name).join(', ');
-    }).catch(function (e) {
-      console.error(e);
-    });
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        //Add the details to the item
+        item.imageUrl = details.sprites.other.dream_world.front_default;
+        item.height = details.height;
+        item.weight = details.weight;
+        item.types = details.types.map((type) => type.type.name).join(', ');
+        item.abilities = details.abilities
+          .map((ability) => ability.ability.name)
+          .join(', ');
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
   }
 
   //Modal section
@@ -68,7 +75,6 @@ let pokemonRepository = (function () {
   function showModal(pokemon) {
     let modalBody = $('.modal-body');
     let modalTitle = $('.modal-title');
-
     modalTitle.empty();
     modalBody.empty();
     let nameElement = $('<h1>' + pokemon.name + '</h1>');
@@ -77,15 +83,15 @@ let pokemonRepository = (function () {
     let heightElement = $('<p>' + 'Height : ' + pokemon.height + '</p>');
     let weightElement = $('<p>' + 'Weight : ' + pokemon.weight + '</p>');
     let typeElement = $('<p>' + 'Types : ' + pokemon.types + '</p>');
-    let abilitiesElement = $('<p>' + 'Abilities : ' + pokemon.abilities + '</p>');
-
+    let abilitiesElement = $(
+      '<p>' + 'Abilities : ' + pokemon.abilities + '</p>'
+    );
     modalTitle.append(nameElement);
     modalBody.append(imageElement);
     modalBody.append(heightElement);
     modalBody.append(weightElement);
     modalBody.append(typeElement);
     modalBody.append(abilitiesElement);
-
   }
 
   function showDetails(pokemon) {
@@ -100,7 +106,7 @@ let pokemonRepository = (function () {
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails
+    showDetails: showDetails,
   };
 })();
 
@@ -110,7 +116,6 @@ pokemonRepository.loadList().then(function () {
   });
 });
 
-
 //search function
 function searchFunction(event) {
   let pokemonNames = document.getElementsByClassName('pokemonButton');
@@ -118,7 +123,6 @@ function searchFunction(event) {
   let searchQuery = value.toLowerCase();
   for (let pokemonName of pokemonNames) {
     let name = pokemonName.textContent.toLowerCase();
-    //display pokemon name if it contains value inside of search box
     if (name.includes(searchQuery)) {
       pokemonName.closest('li').style.display = 'inline-block';
     } else {
